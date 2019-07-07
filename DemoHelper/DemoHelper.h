daa94a8560d730e7ee7b08d90b69374627630223
liujie8 <cjsliuj@163.com>
2019-07-07 22:50:57 +0800

    Initial Commit

---
+++ DemoHelper/DemoHelper.h
@@ -0,0 +1,19 @@
+//
+//  DemoHelper.h
+//  DemoHelper
+//
+//  Created by 刘杰 on 2019/7/7.
+//  Copyright © 2019 jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoHelper.
+FOUNDATION_EXPORT double DemoHelperVersionNumber;
+
+//! Project version string for DemoHelper.
+FOUNDATION_EXPORT const unsigned char DemoHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoHelper/PublicHeader.h>
+
+
